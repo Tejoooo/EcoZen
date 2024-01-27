4a701eea1ff96baf9fbe233647aa73127d6db2a7
praveen <cbsrirangapraveen.2004@gmail.com>
2024-01-27 17:38:48 +0530

    verification added done

--- ecozen/lib/pages/services/pagesService.dart
+++ ecozen/lib/pages/services/pagesService.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: non_constant_identifier_names, use_build_context_synchronously
+
 import 'package:ecozen/pages/account.dart';
 import 'package:ecozen/pages/heatmaps.dart';
 import 'package:ecozen/pages/homePage.dart';
 import 'package:ecozen/pages/post.dart';
+import 'package:ecozen/pages/services/snackBar.dart';
 import 'package:ecozen/pages/zencoins.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../controllers/bottomnavbar.dart';
@@ -17,10 +21,15 @@ class PagesService extends StatefulWidget {
 class _PagesServiceState extends State<PagesService> {
   int _currentIndex = 0;
   late final List<Widget> _pages;
+  bool isEmailVerified = false;
 
   @override
   void initState() {
     super.initState();
+    isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    if (!isEmailVerified) {
+      SendVerificationMail();
+    }
     _pages = [
       HomePage(),
       HeatMaps(),
@@ -28,6 +37,21 @@ class _PagesServiceState extends State<PagesService> {
       ZenCoins(),
       Account()
     ];
+  }
+
+  void SendVerificationMail() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser!;
+      await user.sendEmailVerification();
+    } catch (e) {
+      ErrorSnackBar(context, e.toString());
+    }
+  }
+
+  void checkVerificationOfEmail() async {
+    setState(() {
+      isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
   }
 
   void onItemTapped(int index) {
@@ -38,9 +62,37 @@ class _PagesServiceState extends State<PagesService> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: _pages[_currentIndex],
-        bottomNavigationBar: MyBottomNavigationBar(
-            currentIndex: _currentIndex, onItemTapped: onItemTapped));
+    return isEmailVerified
+        ? Scaffold(
+            body: _pages[_currentIndex],
+            bottomNavigationBar: MyBottomNavigationBar(
+                currentIndex: _currentIndex, onItemTapped: onItemTapped))
+        : Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: SendVerificationMail,
+                    child: Text("Send Verification Mail"),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  ElevatedButton(
+                    onPressed: checkVerificationOfEmail,
+                    child: Text("Verify"),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("SignOut"),
+                  ),
+                ],
+              ),
+            ),
+          );
   }
 }
